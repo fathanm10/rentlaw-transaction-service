6825d09f2db95f602ed6e3765a32b6b67a7965ea
fathanm10 <fathan.muhammad01@ui.ac.id>
2023-04-13 19:54:51 +0700

    init

---
+++ src/main/java/com/rentlaw/transactionservice/RentlawTransactionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rentlaw.transactionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentlawTransactionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentlawTransactionServiceApplication.class, args);
+    }
+
+}
